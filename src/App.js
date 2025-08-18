23c18e96b3efa5b1f63e0116311339887d4367f3
ShahinaShanavas <shahinashanavas353@gmail.com>
2025-08-18 21:26:52 +0530

    user commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Campaigns from './Components/Campaigns';
+
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Donate from './pages/Donate';
+import Donation_History from './pages/Donation_History';
+import DonatePage from './pages/DonatePage'; // ✅ New page for selecting campaign & form
+import DonationFormPage from './pages/DonationFormPage';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+
+      <Routes>
+        
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <Campaigns />
+              
+            </>
+          }
+        />
+
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/donate" element={<Donate />} />
+        <Route path="/donation_history" element={<Donation_History />} />
+
+        {/* New donate page with campaign search and donation form */}
+        <Route path="/donate-campaigns" element={<DonatePage />} />
+        <Route path="/donation-form" element={<DonationFormPage />} />
+        
+
+        
+      </Routes>
+    </Router>
   );
 }
 
